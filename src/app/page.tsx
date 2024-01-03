faa62a26c053f27af461c88d4eba4ff3a3f9e439
kawasaki <iduchungho@gmail.com>
2024-01-04 00:12:50 +0700

    feat: add more component for navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import React from "react"
-import NavbarComponent from "@/components/navbar"
 
 export default function Home() {
   return (
     <div>
-      <NavbarComponent />
+
     </div>
   )
 }
