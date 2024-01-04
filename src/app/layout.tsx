6de38ed2b421e7e793fa542df5788cd43b18b7b9
kawasaki <iduchungho@gmail.com>
2024-01-04 14:02:51 +0700

    feat: update landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import NavbarComponent from "@/components/navbar";
+import FooterComponent from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Providers>
           <NavbarComponent />
           {children}
+          <FooterComponent />
         </Providers>
       </body>
     </html>
