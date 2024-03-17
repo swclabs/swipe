8dba186eff6569461be9b25284ba2d663d79b49d
Duc-Hung Ho <iduchungho@gmail.com>
2024-03-17 13:30:42 +0700

    chore: disable mobile screen (#102)

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,17 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
+import { isMobile } from 'react-device-detect';
+import { useWindowSize } from "@/utils/window-size";
 
 const Providers = ({ children }: any) => {
+    const size = useWindowSize()
+    if (isMobile) {
+        return (
+            <div className=" flex h-[100vh] flex-col text-center justify-center text-xs">
+                <p className="">Application does not support this device</p>
+            </div>
+        );
+    }
     return (
         <NextUIProvider>
             {children}
