8f96ef95616dbb91ff63b89b77e4ef1a600b5c9d
kawasaki <iduchungho@gmail.com>
2024-04-24 13:14:37 +0700

    feat: add admin dash to app

---
+++ x/admin/src/components/dashboard/main-nav.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+
+export function MainNav({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <nav
+      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
+      {...props}
+    >
+      <Link
+        href="/dashboard"
+        className="text-sm font-medium transition-colors hover:text-primary"
+      >
+        Overview
+      </Link>
+      <Link
+        href="/dashboard"
+        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
+        Customers
+      </Link>
+      <Link
+        href="/dashboard"
+        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
+        Products
+      </Link>
+      <Link
+        href="/mail"
+        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
+        Mail
+      </Link>
+      <Link
+        href="/forms"
+        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
+        Settings
+      </Link>
+    </nav>
+  )
+}
