c2793ee0dfd72e069a08b91a2b1946c0ff7365e8
anthonyynguyen <anthony2742000@gmail.com>
2024-05-09 10:35:52 +0700

    Fixing UI in accessroies page

--- src/components/accessories/iphone-accessories.tsx
+++ src/components/accessories/iphone-accessories.tsx
@@ -6,13 +6,13 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
 
 export default function AccessoryIphone() {
     return (
-        <div className="2xl:w-3/5 w-full m-auto">
-            <Carousel className="w-full">
-                <CarouselContent>
+        <div className="2xl:w-3/5 w-[70%] m-auto">
+            <Carousel className="w-full h-full">
+                <CarouselContent className="h-full">
                     {iPhoneAccessories.map((value, index) => (
                         <CarouselItem key={index} className="basis-1/3">
-                            <div className="bg-slate-100 w-full h-[50vh] text-black rounded-xl flex flex-col justify-center items-center" key={index}>
-                                <div className="w-full h-[50%] flex justify-center items-center">
+                            <div className="bg-slate-100 w-full h-full py-4 text-black rounded-xl flex flex-col justify-between items-center" key={index}>
+                                <div className="w-full 2xl:h-[60%] xl:h-[60%] lg:h-[60%] h-[40%] flex justify-center items-center">
                                     <Image
                                         alt=""
                                         className="h-full object-contain"
@@ -30,11 +30,9 @@ export default function AccessoryIphone() {
                                         />
                                     ))}
                                 </div>
-                                <div className="flex flex-col justify-center items-center gap-4 p-4">
-                                    <p className="2xl:text-sm text-xs text-red-500 h-[3vh]">{value.tag}</p>
-                                    <p className="text-center 2xl:text-base xl:text-sm text-xs font-semibold h-[8vh]">{value.name}</p>
-                                    <p className="2xl:text-sm text-xs font-light">{value.price}</p>
-                                </div>
+                                <p className="2xl:text-sm text-xs text-red-500 h-3">{value.tag}</p>
+                                <p className="text-center h-20 2xl:text-lg text-sm font-semibold 2xl:px-8 px-4">{value.name}</p>
+                                <p className="2xl:text-sm text-xs font-light">{value.price}</p>
                             </div>
                         </CarouselItem>
                     ))}
