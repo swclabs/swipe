8976b060dfe826fd3fc577b5a62e8fe37b6c8cba
kawasaki <iduchungho@gmail.com>
2024-07-05 14:51:35 +0700

    chore: add inventory component

--- plugin/admin/src/app/dashboard/shop/inventory/page.tsx
+++ plugin/admin/src/app/dashboard/shop/inventory/page.tsx
@@ -1,36 +1,48 @@
 'use client'
+import { SearchDialog } from "@/components/inventory/dialog";
 import { SelectDemo } from "@/components/inventory/select";
+import { TableDemo } from "@/components/inventory/table";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Search } from "lucide-react";
+import { PlusCircle, Search } from "lucide-react";
 
 
 export default function Page() {
   return (
     <ScrollArea className="h-full bg-muted/40">
-      <div className="p-5">
-        <h1 className=" text-2xl font-semibold py-3">Inventory</h1>
-        <div className=" text-sm font-semibold text-gray-500 flex space-x-3 pb-3">
-          <Badge>All</Badge> <Badge>Active</Badge> <Badge>Draft</Badge> <Badge>Archived</Badge>
-        </div>
-        <div className=" flex pb-3 w-[300px] items-center">
-          <Button variant={"outline"} size={"default"} className="w-full justify-start">
-            <div className="flex space-x-2 items-center">
-              <Search className="w-4" />
-              <div className=" text-sm">
-                Search
-              </div>
+      <div className="container">
+        <div className=" container">
+          <div className="flex items-center justify-between py-3">
+            <h1 className=" text-2xl font-semibold">Inventory</h1>
+            <div className="flex space-x-3">
+              <Button size="sm" className="h-8 gap-1">
+                <PlusCircle className="h-3.5 w-3.5" />
+                <div className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                  Add
+                </div>
+              </Button>
+              <SearchDialog />
             </div>
-          </Button>
+          </div>
+          <div className=" text-sm font-semibold text-gray-500 flex space-x-3 pb-3">
+            <Badge variant={"secondary"}>All</Badge>
+            <Badge variant={"default"}>Active</Badge>
+            <Badge variant={"outline"}>Draft</Badge>
+            <Badge variant={"destructive"}>Archived</Badge>
+          </div>
+          <div className=" flex space-x-3">
+            <SelectDemo />
+            <SelectDemo />
+            <SelectDemo />
+            <SelectDemo />
+            <Button size={"default"}>Filter</Button>
+          </div>
         </div>
-        <div className=" flex space-x-3">
-          <SelectDemo />
-          <SelectDemo />
-          <SelectDemo />
-          <SelectDemo />
-          <Button size={"default"}>Filter</Button>
+
+        <div className=" container py-5">
+          <TableDemo />
         </div>
       </div>
     </ScrollArea >
