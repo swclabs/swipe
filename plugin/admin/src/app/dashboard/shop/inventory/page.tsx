75236dd9d9724440618b96d62f01b9caf4b69e88
kawasaki <iduchungho@gmail.com>
2024-07-19 21:54:00 +0700

    feat: update inventory page

--- plugin/admin/src/app/dashboard/shop/inventory/page.tsx
+++ plugin/admin/src/app/dashboard/shop/inventory/page.tsx
@@ -4,9 +4,10 @@ import { SelectDemo } from "@/components/inventory/select";
 import { InventoryTableComponent } from "@/components/inventory/table";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { PlusCircle, Search } from "lucide-react";
+import { Group, PlusCircle, Search, ShoppingBag, Package, Truck } from "lucide-react";
 
 
 export default function Page() {
@@ -16,32 +17,73 @@ export default function Page() {
         <div className=" container">
           <div className="flex items-center justify-between py-3">
             <h1 className=" text-2xl font-semibold">Inventory</h1>
-            <div className="flex space-x-3">
-              <Button size="sm" className="h-8 gap-1">
-                <PlusCircle className="h-3.5 w-3.5" />
-                <div className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                  Add
-                </div>
-              </Button>
-              <SearchDialog />
-            </div>
           </div>
-          <div className=" text-sm font-semibold text-gray-500 flex space-x-3 pb-3">
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 py-2">
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-semibold">
+                  Items
+                </CardTitle>
+                <ShoppingBag />
+              </CardHeader>
+              <CardContent className="grid gap-1">
+                <p className="text-xs text-muted-foreground">
+                  Add new item
+                </p>
+                <Button className=" w-1/2" size="sm">New</Button>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-semibold">
+                  Products
+                </CardTitle>
+                <Package />
+              </CardHeader>
+              <CardContent className="grid gap-1">
+                <p className="text-xs text-muted-foreground">
+                  Add new product
+                </p>
+                <Button className="w-1/2" size="sm">New</Button>
+              </CardContent >
+            </Card>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-semibold">
+                  Category
+                </CardTitle>
+                <Group />
+              </CardHeader>
+              <CardContent className="grid gap-1">
+                <p className="text-xs text-muted-foreground">
+                  Add new category
+                </p>
+                <Button className="w-1/2" size="sm">New</Button>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-semibold">
+                  Suppiler
+                </CardTitle>
+                <Truck />
+              </CardHeader>
+              <CardContent className="grid gap-1">
+                <p className="text-xs text-muted-foreground">
+                  Add new supplier
+                </p>
+                <Button className="w-1/2" size="sm">New</Button>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+        <div className=" container py-5">
+          <div className=" text-sm font-semibold text-gray-500 flex space-x-3">
             <Badge variant={"secondary"}>All</Badge>
             <Badge variant={"default"}>Active</Badge>
             <Badge variant={"outline"}>Draft</Badge>
             <Badge variant={"destructive"}>Archived</Badge>
           </div>
-          <div className=" flex space-x-3">
-            <SelectDemo />
-            <SelectDemo />
-            <SelectDemo />
-            <SelectDemo />
-            <Button size={"default"}>Filter</Button>
-          </div>
-        </div>
-
-        <div className=" container py-5">
           <InventoryTableComponent />
         </div>
       </div>
