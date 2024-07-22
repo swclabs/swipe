5c4538460ec98b02a7abd8582a8c12b9dd4a11ee
anthonyynguyen <anthony2742000@gmail.com>
2024-07-22 14:37:33 +0700

    Fixing CSS /accessories page

--- src/components/accessories/iphone-accessories.tsx
+++ src/components/accessories/iphone-accessories.tsx
@@ -10,9 +10,9 @@ export default function AccessoryIphone() {
             <Carousel className="w-full h-full">
                 <CarouselContent className="h-full">
                     {iPhoneAccessories.map((value, index) => (
-                        <CarouselItem key={index} className="basis-1/3">
+                        <CarouselItem key={index} className="md:basis-1/3">
                             <div className="bg-slate-100 w-full h-full py-4 text-black rounded-xl flex flex-col justify-between items-center" key={index}>
-                                <div className="w-full 2xl:h-[60%] xl:h-[60%] lg:h-[60%] h-[40%] flex justify-center items-center">
+                                <div className="w-full 2xl:h-[60%] xl:h-[60%] lg:h-[60%] md:h-[40%] h-[50%] flex justify-center items-center">
                                     <Image
                                         alt=""
                                         className="h-full object-contain"
@@ -31,8 +31,8 @@ export default function AccessoryIphone() {
                                     ))}
                                 </div>
                                 <p className="2xl:text-sm text-xs text-red-500 h-3">{value.tag}</p>
-                                <p className="text-center h-20 2xl:text-lg text-sm font-semibold 2xl:px-8 px-4">{value.name}</p>
-                                <p className="2xl:text-sm text-xs font-light">{value.price}</p>
+                                <p className="text-center md:h-20 h-12 2xl:text-lg text-sm font-semibold 2xl:px-8 px-4">{value.name}</p>
+                                <p className="2xl:text-sm md:text-xs text-base font-light">{value.price}</p>
                             </div>
                         </CarouselItem>
                     ))}
