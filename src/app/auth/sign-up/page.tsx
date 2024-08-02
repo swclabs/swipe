0756f9b9a2d8bd9f353ad61c4a2936e251577bb0
kawasaki <iduchungho@gmail.com>
2024-08-02 11:00:38 +0700

    refactor: structure manager & client

--- src/app/auth/sign-up/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import React from "react";
-export default function SignUp() {
-    return (
-        <div></div>
-    )
-}
--- src/app/shop/earphone/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-
-
-
-export default function Page({ params }: { params: { slug: string } }) {
-    return (
-        <div>Web ${params.slug} update coming soon...</div>
-    )
-}
