ca798a7ba6f5302d33da7d1712fcb02d98919776
kawasaki <iduchungho@gmail.com>
2024-08-21 22:55:37 +0700

    feat: update comment component in shop

--- client/src/components/shop/spec-memory.tsx
+++ client/src/components/shop/spec-memory.tsx
@@ -24,46 +24,62 @@ const disableSpec = {
 
 const enableSpec = {}
 
-export default function SpecMemory({ product, version, color, specs, setSpecs }: IStorageProps) {
-  const RAM = product[version]?.color[color]?.specs
+const GetByRam = (product: ProductDetail<Specs>[], version: number, color: number) => {
+  return product[version]?.color[color]?.specs
     ?.reduce<string[]>((acc, curr) => {
       if (!acc.includes(curr.RAM)) {
         acc.push(curr.RAM);
       }
       return acc;
     }, []);
+}
+
+export default function SpecMemory({ product, version, color, specs, setSpecs }: IStorageProps) {
   return (
     <>
-      {version !== -1 && color !== -1 &&
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.25 }}
-        >
-          <div className=" container mt-10" id='RAM'>
-            <div className=' font-semibold text-2xl'>
-              Memory. <span className=' text-gray-400'>Bạn cần bao nhiêu memory?</span>
-            </div>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.25 }}
+      >
+        <div className=" container mt-10" id='RAM'>
+          <div className=' font-semibold text-2xl'>
+            Memory. <span className=' text-gray-400'>Bạn cần bao nhiêu memory?</span>
+          </div>
 
-            <div className='pt-2 flex gap-2' style={version === -1 ? disableSpec : enableSpec}>
-              {RAM?.map((value, index) =>
-                <Link href="#RAM" key={index}>
+          <div className='pt-2 flex gap-2' style={version === -1 ? disableSpec : enableSpec}>
+            {
+              version == -1 || color == -1 ?
+                GetByRam(product, 0, 0)?.map((value, index) =>
                   <Button
                     variant="outline"
-                    className={cn(specs.RAM === value ? "border-blue-500" : "border-gray-400")}
+                    className={cn("border-[1.5px]", specs.RAM === value ? "border-blue-500" : "border-gray-400")}
+                    key={index}
+                    onClick={() => setSpecs({ RAM: value, SSD: '', price: '' })}
+                    disabled={color === -1 ? true : false}
+                  >
+                    <Link href="#RAM" key={index}>
+                      {value}
+                    </Link>
+                  </Button>
+                ) :
+                GetByRam(product, version, color)?.map((value, index) =>
+                  <Button
+                    variant="outline"
+                    className={cn("border-[1.5px]", specs.RAM === value ? "border-blue-500" : "border-gray-400")}
                     key={index}
                     onClick={() => setSpecs({ RAM: value, SSD: '', price: '' })}
                   >
-                    {value}
+                    <Link href="#RAM" key={index}>
+                      {value}
+                    </Link>
                   </Button>
-                </Link>
-              )
-              }
+                )
+            }
 
-            </div>
-          </div >
-        </motion.div >
-      }
+          </div>
+        </div >
+      </motion.div >
     </>
   )
 }
