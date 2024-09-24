fcc97d7e2b8cd4abf653400c93919d4cbbe683c9
anthonyynguyen <anthony2742000@gmail.com>
2024-09-24 13:21:02 +0700

    Initialize function Edit, Delete, Add for Inventories

--- manager/src/components/inventory/table.tsx
+++ manager/src/components/inventory/table.tsx
@@ -177,6 +177,8 @@ export const columns: ColumnDef<StockItem>[] = [
             <DropdownMenuSeparator />
             <DropdownMenuItem>View customer</DropdownMenuItem>
             <DropdownMenuItem>View payment details</DropdownMenuItem>
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuItem>Delete</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       )
