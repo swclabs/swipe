2a31715c129849b9f843f36c64523136c16d1ef4
kawasaki <iduchungho@gmail.com>
2024-10-19 20:51:35 +0700

    fix lint

--- client/src/components/content/shop/delivery.tsx
+++ client/src/components/content/shop/delivery.tsx
@@ -13,7 +13,7 @@ export default function Delivery() {
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make changes to your profile here. Click save when you are done.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
