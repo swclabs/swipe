ff01b4a05caa03dc9772674bb1f42caacadc988e
anthonyynguyen <anthony2742000@gmail.com>
2024-10-20 09:35:52 +0700

    Adding Edit profile button

--- client/src/app/profile/account/page.tsx
+++ client/src/app/profile/account/page.tsx
@@ -3,8 +3,12 @@ import React from "react";
 // import { Input } from "@nextui-org/react";
 import { User } from "lucide-react";
 import { Button } from "@nextui-org/react";
+import { useState } from "react";
+import { EditDialog } from "@/components/forms/responsive-dialog";
 
 export default function AccountPage() {
+
+    const [isEditOpen, setIsEditOpen] = useState(false);
 
     const user = {
         first_name: "Khoa",
@@ -18,61 +22,70 @@ export default function AccountPage() {
     }
 
     return (
-        <div className="mt-8 w-full bg-white mb-20">
-            <div className="container mx-auto flex flex-col md:flex-row items-center justify-center w-full bg-gray-100 rounded-lg border p-8 gap-4 md:gap-36">
-                <div className="flex justify-center">
-                    <div className=" flex justify-center items-center cursor-pointer rounded-full border w-fit p-4" onClick={handleUpload}>
-                        <input id="image" className="hidden" type="file">
+        <>
+            <EditDialog
+                isOpen={isEditOpen}
+                setIsOpen={setIsEditOpen}
+            />
+            <div className="mt-8 w-full bg-white mb-20">
+                <div className="container mx-auto flex flex-col md:flex-row items-center justify-center w-full bg-gray-100 rounded-lg border p-8 gap-4 md:gap-36">
+                    <div className="flex justify-center">
+                        <div className=" flex justify-center items-center cursor-pointer rounded-full border w-fit p-4" onClick={handleUpload}>
+                            <input id="image" className="hidden" type="file">
 
-                        </input>
-                        <User size={70} />
+                            </input>
+                            <User size={70} />
+                        </div>
                     </div>
-                </div>
-                <div className="flex items-center">
-                    <div className="w-full flex flex-col gap-4 md:gap-2 pt-2">
-                        <div className="flex flex-col justify-between border-b-2 pb-4 md:pb-2">
-                            <div className="text-sm">
-                                First name
+                    <div className="flex items-center">
+                        <div className="w-full flex flex-col gap-4 md:gap-2 pt-2">
+                            <div className="flex flex-col justify-between border-b-2 pb-4 md:pb-2">
+                                <div className="text-sm">
+                                    First name
+                                </div>
+                                <div className="font-semibold">
+                                    {user.first_name}
+                                </div>
                             </div>
-                            <div className="font-semibold">
-                                {user.first_name}
+                            <div className="flex flex-col justify-between border-b-2 pb-4 md:pb-2">
+                                <div className="text-sm">
+                                    Last name
+                                </div>
+                                <div className="font-semibold">
+                                    {user.last_name}
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex flex-col justify-between border-b-2 pb-4 md:pb-2">
-                            <div className="text-sm">
-                                Last name
+                            <div className="flex flex-col justify-between border-b-2 pb-4 md:pb-2">
+                                <div className="text-sm">
+                                    Email
+                                </div>
+                                <div className="font-semibold">
+                                    {user.email}
+                                </div>
                             </div>
-                            <div className="font-semibold">
-                                {user.last_name}
+                            <div className="flex flex-col justify-between">
+                                <div className="text-sm">
+                                    Phone Number
+                                </div>
+                                <div className="font-semibold">
+                                    {user.phone_number}
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex flex-col justify-between border-b-2 pb-4 md:pb-2">
-                            <div className="text-sm">
-                                Email
+                            <div className="flex flex-col justify-between">
+                                <Button
+                                    color="primary"
+                                    onClick={() => setIsEditOpen(!isEditOpen)}
+                                >
+                                    Edit Profile
+                                </Button>
                             </div>
-                            <div className="font-semibold">
-                                {user.email}
-                            </div>
-                        </div>
-                        <div className="flex flex-col justify-between">
-                            <div className="text-sm">
-                                Phone Number
-                            </div>
-                            <div className="font-semibold">
-                                {user.phone_number}
-                            </div>
-                        </div>
-                        <div className="flex flex-col justify-between">
-                            <Button color="primary">
-                                Edit Profile
-                            </Button>
-                        </div>
-                        {/* <p className=" font-semibold"> Last name : {user.last_name}</p>
+                            {/* <p className=" font-semibold"> Last name : {user.last_name}</p>
                         <p className=" font-semibold">Email: {user.email}</p>
                         <p className=" font-semibold">Số điện thoại: {user.phone_number}</p> */}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div >
+            </div >
+        </>
     );
 }
--- client/src/app/profile/order/page.tsx
+++ client/src/app/profile/order/page.tsx
@@ -1,29 +1,104 @@
 "use client"
 import React from "react";
-import {
-    Table,
-    TableHeader,
-    TableBody,
-    TableColumn,
-    TableRow,
-    TableCell
-} from "@nextui-org/table";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue } from "@nextui-org/react";
+import data from "@/faker/mac";
 
-const TableHeaderRow = () => {
-    return (
-        <TableRow>
-            <TableHeader>
-                <TableColumn>ID</TableColumn>
-            </TableHeader>
-            <TableHeader>
-                <TableColumn>Product</TableColumn>
-            </TableHeader>
-            <TableHeader>
-                <TableColumn>Quantity</TableColumn>
-            </TableHeader>
-        </TableRow>
-    )
-}
+
+const rows = [
+    {
+        delevery_id: 1,
+        product: [
+            {
+                inventory_id: 1,
+                quantity: 1,
+                specs_id: 1,
+            },
+            {
+                inventory_id: 2,
+                quantity: 2,
+                specs_id: 2,
+            }
+        ]
+    },
+    {
+        delevery_id: 2,
+        product: [
+            {
+                inventory_id: 1,
+                quantity: 1,
+                specs_id: 1,
+            },
+            {
+                inventory_id: 2,
+                quantity: 2,
+                specs_id: 2,
+            }
+        ]
+    },
+    {
+        delevery_id: 3,
+        product: [
+            {
+                inventory_id: 1,
+                quantity: 1,
+                specs_id: 1,
+            },
+            {
+                inventory_id: 2,
+                quantity: 2,
+                specs_id: 2,
+            }
+        ]
+    }
+];
+
+const columns = [
+    {
+        key: "delevery_id",
+        label: "Delivery ID",
+    },
+    {
+        key: "product",
+        label: "Product",
+    },
+    {
+        key: "quantity",
+        label: "Quantity",
+    },
+];
+
+const order = [
+    {
+        delevery_id: 1,
+        product: [
+            {
+                inventory_id: 1,
+                quantity: 1,
+                specs_id: 1,
+            },
+            {
+                inventory_id: 2,
+                quantity: 2,
+                specs_id: 2,
+            }
+        ]
+    },
+    {
+        delevery_id: 2,
+        product: [
+            {
+                inventory_id: 1,
+                quantity: 1,
+                specs_id: 1,
+            },
+            {
+                inventory_id: 2,
+                quantity: 2,
+                specs_id: 2,
+            }
+        ]
+    }
+]
 
 export default function AccountPage() {
     /*{
@@ -42,43 +117,39 @@ export default function AccountPage() {
         ]
     } */
 
-    const order = [
-        {
-            delevery_id: 1,
-            product: [
-                {
-                    inventory_id: 1,
-                    quantity: 1,
-                    specs_id: 1,
-                },
-                {
-                    inventory_id: 2,
-                    quantity: 2,
-                    specs_id: 2,
-                }
-            ]
-        },
-        {
-            delevery_id: 2,
-            product: [
-                {
-                    inventory_id: 1,
-                    quantity: 1,
-                    specs_id: 1,
-                },
-                {
-                    inventory_id: 2,
-                    quantity: 2,
-                    specs_id: 2,
-                }
-            ]
-        }
-    ]
+
 
     return (
         <div className="mt-8 w-full bg-white mb-20">
-            <div className="container mx-auto flex flex-col md:flex-row items-center justify-center w-full bg-gray-100 rounded-lg border p-8 gap-4 md:gap-36">
+            <div className="container mx-auto">
+                <Table aria-label="Example table with dynamic content">
+                    <TableHeader columns={columns}>
+                        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+                    </TableHeader>
+                    <TableBody items={rows}>
+                        {rows.map((row, index) => (
+                            <TableRow key={index}>
+                                <TableCell>{row.delevery_id}</TableCell>
+                                <TableCell>{row.product.map((value, index) => (
+                                    <div key={index}>
+                                        <p>{value.inventory_id}</p>
+                                        <p>{value.specs_id}</p>
+                                    </div>
+                                ))}
+                                </TableCell>
+                                <TableCell>{row.product.map((value, index) => (
+                                    <div key={index}>
 
+                                        <p>{value.quantity}</p>
+
+                                    </div>
+                                ))}
+                                </TableCell>
+                            </TableRow>
+                        )
+                        )}
+                    </TableBody>
+                </Table>
             </div>
         </div >
     );
